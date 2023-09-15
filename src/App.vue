00b6909578c19380072042fe68ffdaf4ae200668
chhw130 <527coco@naver.com>
2023-09-15 16:34:11 +0900

    Feat : Trainstion Component Function

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <TheHeader />
-    <BadgeList />
+    <!-- <BadgeList />
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
@@ -12,22 +12,30 @@
         <h2>{{ slotProps.item }}</h2>
         <p>{{ slotProps.anotherProps }}</p>
       </template>
-    </CourseGoal>
+    </CourseGoal> -->
+    <button @click="setSeletecComponent('ActiveGoals')">Active Goals</button>
+    <button @click="setSeletecComponent('ManageGoals')">Manage Goals</button>
+    <ManageGoals v-if="seletedComponent === 'ManageGoals'" />
+    <ActiveGoals v-if="seletedComponent === 'ActiveGoals'" />
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
-import CourseGoal from "./components/CourseGoal.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoal from "./components/CourseGoal.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
 
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoal,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoal,
+    ManageGoals,
+    ActiveGoals,
   },
   data() {
     return {
@@ -36,7 +44,16 @@ export default {
         description: "Site owner and admin",
         role: "admin",
       },
+      seletedComponent: ManageGoals,
     };
+  },
+
+  methods: {
+    setSeletecComponent(cmp) {
+      this.seletedComponent = cmp;
+
+      console.log(this.seletedComponent);
+    },
   },
 };
 </script>
