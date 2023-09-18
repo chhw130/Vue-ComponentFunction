b832b8ec7a3457e68ce0d12d32c65936d422a2c1
chhw130 <527coco@naver.com>
2023-09-18 10:22:14 +0900

    Feat : ProductStore

--- src/store/ProductStore.js
+++ src/store/ProductStore.js
@@ -3,9 +3,5 @@ import { defineStore } from "pinia";
 const data = { id: 1, name: "carousel" };
 
 export const useProductStore = defineStore("ProductStore", {
-  state: () => {
-    return {
-      data,
-    };
-  },
+  state: () => data,
 });
